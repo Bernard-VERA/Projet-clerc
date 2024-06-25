cf749b981a58cd726374eb86ae0f21368dc91e92
Bernard-VERA <bvmars@hotmail.com>
2024-06-26 00:16:48 +0200

    Mise à jour Header et Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import '../styles/Footer.css';
     return(
         <footer className="footer">
             <img src={ logo_white} className='footer-logo' alt='logo Kasa' />
-            <p className='footer-text'><span>© 2024 Le clerc.</span> <span>Tous droits réservés</span></p>
+            <p className='footer-text'><span>© 2024 Le clerc.</span><span> Tous droits réservés</span></p>
         </footer>
     )
 }
