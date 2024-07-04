e0a1342153c5283628d0ff37e87efa8507e30e92
Bernard-VERA <bvmars@hotmail.com>
2024-07-05 00:35:30 +0200

    Création des 3 premiers articles

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Routes, Route,} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Article from './pages/Article';
+import './styles/GlobalStyle.css';
 
 function App() {
   return (
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -3,6 +3,7 @@ import articles from '../data/articles.json';
 import { useParams } from 'react-router-dom';
 import '../styles/Article.css';
 import Error from './Error';
+import { NavLink } from "react-router-dom";
 
 
 // useParams est importé avec tous les autres composants, notament "articles.json"
@@ -18,21 +19,21 @@ const Article = () => {
 
   return (
       <section className='article-page'>
-      <img src = {article.picture} alt="detail"/>
+      <img src = {article.picture} alt="illustration"/>
         <header className='article-page-header'>
                 <article className='article-page-header-infos'>
                     <p className='article-page-header-infos-title'>{article.title}</p>
-                    <p className='article-page-header-infos-subtitle'>{article.description}</p>
-                    
-                </article> 
-            
-                
+                    <p className='article-page-header-infos-subtitle'>{article.description1}</p>
+                    <p className='article-page-header-infos-subtitle'>{article.description2}</p>
+                    <p className='article-page-header-infos-subtitle'>{article.description3}</p>
+                    <p className='article-page-header-infos-subtitle'>{article.description4}</p>
+                </article>   
             </header>
-
-            
+            <main className='article-main'>
+            <NavLink className="article-page-link" title="Accueil" end to="/">Retourner sur la page d'accueil</NavLink>
+            </main>  
         </section>
     )
 }
-// Dans "Header" et "Equipement" les paramètres sont récupérés par "map" qui crée un tableau, avec les listes créées par "key" (props) 
 
 export default Article;
