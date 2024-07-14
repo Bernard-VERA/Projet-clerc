36db49d3935417dabc21cd1b92c7bf9ff172b06a
Bernard-VERA <bvmars@hotmail.com>
2024-07-14 03:08:30 +0200

    Ajout de 2 nouveaux articles

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -10,30 +10,30 @@ import { NavLink } from "react-router-dom";
 
 const Article = () => {
 
-    // Le hook "useParams" permet d'extraire les paramètres des logements (id) 
+    // Le hook "useParams" permet d'extraire les paramètres des articles (id) 
   const { id } = useParams()
   const article = articles.find(article => article.id === id)
   if (article === undefined) {
     return <Error />
-  } // Si le logement est "undefined" on est renvoyé vers la page d'erreur
+  } // Si l'article est "undefined" on est renvoyé vers la page d'erreur
 
   return (
       <section className='article-page'>
-      <img src = {article.picture} alt="illustration"/>
-        <header className='article-page-header'>
-                <article className='article-page-header-infos'>
-                    <p className='article-page-header-infos-title'>{article.title}</p>
-                    <p className='article-page-header-infos-subtitle'>{article.description1}</p>
-                    <p className='article-page-header-infos-subtitle'>{article.description2}</p>
-                    <p className='article-page-header-infos-subtitle'>{article.description3}</p>
-                    <p className='article-page-header-infos-subtitle'>{article.description4}</p>
-                    <p className='article-page-header-infos-subtitle'>{article.description5}</p>
-                </article>   
-            </header>
-            <main className='article-main'>
+        <img src = {article.picture} alt="illustration"/>
+          <header className='article-page-header'>
+              <article className='article-page-header-infos'>
+                  <p className='article-page-header-infos-title'>{article.title}</p>
+                  <p className='article-page-header-infos-subtitle'>{article.description1}</p>
+                  <p className='article-page-header-infos-subtitle'>{article.description2}</p>
+                  <p className='article-page-header-infos-subtitle'>{article.description3}</p>
+                  <p className='article-page-header-infos-subtitle'>{article.description4}</p>
+                  <p className='article-page-header-infos-subtitle'>{article.description5}</p>
+              </article>   
+          </header>
+          <main className='article-main'>
             <NavLink className="article-page-link" title="Accueil" end to="/">Retourner sur la page d'accueil</NavLink>
-            </main>  
-        </section>
+          </main>  
+      </section>
     )
 }
 
