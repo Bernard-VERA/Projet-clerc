88df119bdeb3eebf2e4445707dd88ee9b5e90517
Bernard-VERA <bvmars@hotmail.com>
2024-08-18 00:34:34 +0200

    Annulation changement auto de date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import logo_white from '../assets/logo_white.png'
 import '../styles/Footer.css';
 
-/* Permet de changer l'année du copyright automatiquement le 31 décembre */
- const year = new Date().getFullYear();
- document.querySelector('#year').textContent= year; 
-
  const Footer = () => {
     return(
         <footer className="footer">
             <img src={ logo_white} className='footer-logo' alt='logo Kasa' />
-            <p className='footer-text'>© <span id="year"></span> Le clerc.<br></br>Tous droits réservés</p>
+            <p className='footer-text'><span>© 2024 Le clerc.<br></br>Tous droits réservés</span></p>
         </footer>
     )
 }
