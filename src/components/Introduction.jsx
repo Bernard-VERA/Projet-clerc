bc1dbaa92e9e7022986451e2205327f5e9602a73
Bernard-VERA <bvmars@hotmail.com>
2024-08-21 22:33:00 +0200

    Modification de favicon et de 2 erreurs

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,7 +6,7 @@ const Introduction = () => {
         <section className="intro">
             <h2 className="intro-title">Bienvenue sur le Portail des clercs du Commissaire de Justice
             </h2>
-            <p className="intro-text">Ce nouveau site web est dédié aux employés du commissaire de justice. Ce portail a été conçu pour vous offrir un accès facile et rapide, à toutes les informations et ressources dont vous avez besoin dans le cadre de votre travail quotidien. Vous trouverez ici les documents essentiels pour votre travail. Il s'agit de documents basés sur la convention collective de votre profession. Nous espérons que ce site deviendra un outil indispensable dans votre quotidien professionnel. Bonne navigation
+            <p className="intro-text">Ce site web est dédié aux employés du commissaire de justice. Ce portail a été conçu pour vous offrir un accès facile et rapide, à toutes les informations et ressources dont vous avez besoin dans le cadre de votre travail quotidien. Vous trouverez ici les documents essentiels pour votre travail. Il s'agit de documents basés sur la convention collective de votre profession. Nous espérons que ce site deviendra un outil indispensable dans votre quotidien professionnel. Bonne navigation
             </p>
         </section>
     )
