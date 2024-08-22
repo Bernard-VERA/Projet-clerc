6067ba55216176dbba465821c845581dde451385
Bernard-VERA <bvmars@hotmail.com>
2024-08-22 15:53:00 +0200

    Correction du texte d'introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,7 +6,7 @@ const Introduction = () => {
         <section className="intro">
             <h2 className="intro-title">Bienvenue sur le Portail des clercs du Commissaire de Justice
             </h2>
-            <p className="intro-text">Ce site web est dédié aux employés du commissaire de justice. Ce portail a été conçu pour vous offrir un accès facile et rapide, à toutes les informations et ressources dont vous avez besoin dans le cadre de votre travail quotidien. Vous trouverez ici les documents essentiels pour votre travail. Il s'agit de documents basés sur la convention collective de votre profession. Nous espérons que ce site deviendra un outil indispensable dans votre quotidien professionnel. Bonne navigation
+            <p className="intro-text">Ce site web est dédié aux employés des commissaires de justice. Ce portail a été conçu pour vous offrir un accès facile et rapide, à toutes les informations essentielles, et aux ressources dont vous avez besoin dans le cadre de votre travail quotidien. Il peut aussi être utile à ceux qui désirent entrer dans cette profession. Il s'agit principalement d'articles basés sur la convention collective des commissaires de justice. Nous espérons que ce site vous aidera à améliorer la qualité de votre quotidien professionnel. Bonne navigation
             </p>
         </section>
     )
