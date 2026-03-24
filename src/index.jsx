a8375d711d974990e46b56e292cbbba1010e7aec
Bernard-VERA <bvmars@hotmail.com>
2026-03-24 23:20:17 +0100

    Migration vers Vite réalisée

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -10,6 +10,3 @@ root.render(
     <App />
   </Router>
 );
-
-
-
